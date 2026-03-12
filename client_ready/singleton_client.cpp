b9ff0b88a0a72b92babc03099652047f2cf3832d
He_nix <107463401+Svyatofslav@users.noreply.github.com>
2026-03-12 20:41:30 +0300

    Update singleton_client.cpp

--- client_ready/singleton_client.cpp
+++ client_ready/singleton_client.cpp
@@ -1,42 +1,46 @@
-#include "Singleton_client.h"
+#include "singleton_client.h"
 
-SingletonClient::SingletonClient(QObject *parent) : QObject(parent) 
+// Определения статических переменных — только здесь, не в .h!
+SingletonClient* SingletonClient::p_instance = nullptr;
+SingletonDestroyer SingletonClient::destroyer;
+
+// Деструктор здесь — SingletonClient уже полностью известен
+SingletonDestroyer::~SingletonDestroyer()
 {
-    mTcpSocket = new QTcpSocket(this);
-    mTcpSocket-> connectToHost("127.0.0.1", 33333);
-
-    connect(mTcpSocket, SIGNAL(readyRead()), 
-    this, SLOT(slotServerRead()));
-
-    //connect(mTcpSocket, &QTcpSocket::readyRead, 
-    //this, &SingletonClient::slotServerRead()));
-
+    delete p_instance;
 }
 
-SingletonClient* SingletonClient::getInstance() 
+SingletonClient::SingletonClient(QObject *parent) : QObject(parent)
 {
-    if (!p_instance) 
+    mTcpSocket = new QTcpSocket(this);
+    mTcpSocket->connectToHost("127.0.0.1", 33333);
+    connect(mTcpSocket, &QTcpSocket::readyRead,
+            this, &SingletonClient::slotServerRead);
+}
+
+SingletonClient* SingletonClient::getInstance()
+{
+    if (!p_instance)
     {
-        p_instance = new Singleton();
+        p_instance = new SingletonClient();
         destroyer.initialize(p_instance);
     }
     return p_instance;
 }
 
-// отправить сообщение на сервер
 void SingletonClient::send_msg_to_server(QString query)
 {
     mTcpSocket->write(query.toUtf8());
 }
 
-void SingletonClient::slotServerRead(){
-    //qDebug()<< "slot\n";
+void SingletonClient::slotServerRead()
+{
     QString msg = "";
-    while(mTcpSocket->bytesAvailable()>0)
+    while(mTcpSocket->bytesAvailable() > 0)
     {
-        QByteArray array =mTcpSocket->readAll();
+        QByteArray array = mTcpSocket->readAll();
         msg.append(array);
     }
-    qDebug()<<msg;
+    qDebug() << msg;
     emit message_from_server(msg);
 }
