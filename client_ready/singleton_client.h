53487b4d2b264a185fafb0b1842c9f0f4c47ecb6
He_nix <107463401+Svyatofslav@users.noreply.github.com>
2026-03-12 20:41:47 +0300

    Update singleton_client.h

--- client_ready/singleton_client.h
+++ client_ready/singleton_client.h
@@ -3,64 +3,43 @@
 
 #include <QObject>
 #include <QTcpSocket>
-#include <QtNetwork>
-#include <QByteArray>
 #include <QDebug>
 
 class SingletonClient;
 
-class SingletonDestroyer 
+class SingletonDestroyer
 {
-    private:
-        SingletonClient * p_instance;
-    public:
-        ~SingletonDestroyer() 
-        {
-            delete p_instance;
-        }
-        void initialize(SingletonClient * p) 
-        {
-            p_instance = p;
-        }
+private:
+    SingletonClient * p_instance = nullptr;
+public:
+    ~SingletonDestroyer();                        // ← только объявление
+    void initialize(SingletonClient * p)
+    {
+        p_instance = p;
+    }
 };
 
-class SingletonClient : public QObject 
+class SingletonClient : public QObject
 {
     Q_OBJECT
-    private:
-        static SingletonClient * p_instance;
-        static SingletonDestroyer destroyer;
-        QTcpSocket * mTcpSocket;
-    protected:
-        SingletonClient() {}
-        SingletonClient(const SingletonClient&) = delete;
-        SingletonClient& operator = (SingletonClient &) = delete;
-        ~SingletonClient() {}
-        friend class SingletonDestroyer;
-    public:
-        static SingletonClient* getInstance() 
-        {
-            if (!p_instance) 
-            {
-                p_instance = new Singleton();
-                destroyer.initialize(p_instance);
-            }
-            return p_instance;
-        }
+private:
+    static SingletonClient * p_instance;
+    static SingletonDestroyer destroyer;
+    QTcpSocket * mTcpSocket;
+protected:
+    SingletonClient(QObject *parent = nullptr);
+    SingletonClient(const SingletonClient&) = delete;
+    SingletonClient& operator = (SingletonClient &) = delete;
+    ~SingletonClient() {}
+    friend class SingletonDestroyer;
+public:
+    static SingletonClient* getInstance();
+    void send_msg_to_server(QString query);
 
-        void send_msg_to_server(QString query);
-
-        /*
-    signals:
-        void message_from_server(QString msq);
-    private slots:
-        void slotServerRead();
-        */
-        // Запрос на подключение - в конструкторе
-        // Отправка сообщения на сервер - функция 
-        // 
+signals:
+    void message_from_server(QString msg);
+private slots:
+    void slotServerRead();
 };
 
-// Выделение памяти под статичные переменные
-Singleton::SingletonClient * p_instance;
-Singleton::SingletonDestroyer destroyer;
+#endif // SINGLETON_CLIENT_H
