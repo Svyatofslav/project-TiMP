bc3fa3db67f1258595b3295eb6b3c00686edba81
He_nix <107463401+Svyatofslav@users.noreply.github.com>
2026-03-12 20:40:39 +0300

    Update singleton.h

--- client_ready/singleton.h
+++ client_ready/singleton.h
@@ -1,42 +1,42 @@
 class Singleton;
 
 // Для удаления данных
-class SingletonDestroyer 
+class SingletonDestroyer
 {
-    private:
-        Singleton * p_instance;
-    public:
-        ~SingletonDestroyer() 
-        {
-            delete p_instance;
-        }
-        void initialize(Singleton * p) 
-        {
-            p_instance = p;
-        }
+private:
+    Singleton * p_instance;
+public:
+    ~SingletonDestroyer()
+    {
+        delete p_instance;
+    }
+    void initialize(Singleton * p)
+    {
+        p_instance = p;
+    }
 };
 
 class Singleton
 {
-    private:
-        static Singleton * p_instance;
-        static SingletonDestroyer destroyer;
-    protected:
-        Singleton() {}
-        Singleton(const Singleton&) = delete;
-        Singleton& operator = (Singleton &) = delete;
-        ~Singleton() {}
-        friend class SingletonDestroyer;
-    public:
-        static Singleton* getInstance() 
+private:
+    static Singleton * p_instance;
+    static SingletonDestroyer destroyer;
+protected:
+    Singleton() {}
+    Singleton(const Singleton&) = delete;
+    Singleton& operator = (Singleton &) = delete;
+    ~Singleton() {}
+    friend class SingletonDestroyer;
+public:
+    static Singleton* getInstance()
+    {
+        if (!p_instance)
         {
-            if (!p_instance) 
-            {
-                p_instance = new Singleton();
-                destroyer.initialize(p_instance);
-            }
-            return p_instance;
+            p_instance = new Singleton();
+            destroyer.initialize(p_instance);
         }
+        return p_instance;
+    }
 };
 
 // Выделение памяти под статичные переменные
