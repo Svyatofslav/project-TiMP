4bf941adc4df9b559af5595c2d03b6a0f5e40386
Gleb710 <gleb527gleb@gmail.com>
2026-04-04 19:48:52 +0300

    Dew gleb (#4)
    * сделал отчистку в полях логин и пароль в авторизации
    
    * Добавил дизайн всех окон
    
    ---------
    
    Co-authored-by: Gleb K <gleb527gleb@yandex.ru>

--- Frontend/main.cpp
+++ Frontend/main.cpp
@@ -1,12 +1,14 @@
 #include <QApplication>
 #include "windowmanager.h"
 
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     WindowManager manager;
     manager.showAuth(); // стартуем с окна авторизации
+
 
     return a.exec();
 }
